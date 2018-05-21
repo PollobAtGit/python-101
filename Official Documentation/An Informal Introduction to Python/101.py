ff5ebf78f7605e0f750f54b8cfed41fbe11dbe11
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-05-22 00:24:45 +0600

    Python 101

---
+++ Official Documentation/An Informal Introduction to Python/101.py
@@ -0,0 +1,14 @@
+value = 10
+valueTwo = 20
+
+print(value + valueTwo)
+
+needTrimming = " left trim : right trim "
+
+print(needTrimming.lstrip())
+print(needTrimming.rstrip())
+print(needTrimming.strip())
+
+exponent = 3 ** 9
+
+print(exponent)# 19683
