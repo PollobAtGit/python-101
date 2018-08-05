9e5558a80e51cd71e53a7f13f18c78e58cf9d44e
Sheikh Ashiqul Islam <ashiqul.islam@enosisbd.com>
2018-08-05 15:00:32 +0600

    unit test - tdd - python

---
+++ tdd-with-python/superlists/functional_tests.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_can_get_it_later(self):
+        print(self.browser)
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish Test")
+
+if __name__ == "__main__":
+    unittest.main()
