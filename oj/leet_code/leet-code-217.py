b18baf5c320a6904417035f140117505825d2016
pollob <meet.pollob@gmail.com>
2018-08-09 21:48:59 +0600

    lc - linked list

---
+++ oj/leet_code/leet-code-217.py
@@ -0,0 +1,52 @@
+class Solution:
+    
+    def contains_duplicate_by_sort(self, nums):
+
+        if nums is not None:
+
+            nums.sort()
+
+            for i, _ in enumerate(nums):
+                if (i + 1) < len(nums) and nums[i] == nums[i + 1]:
+                    return True
+
+            return False
+
+
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        if nums is not None:
+
+            dic = {}
+
+            for x in nums:
+                if x is not None:
+                    if dic.get(x):
+                        return True
+                    else:
+                        dic[x] = 1
+
+            return False
+        
+
+s = Solution()
+
+"""
+print(s.containsDuplicate([1,2,3,1]))
+print(s.containsDuplicate([1,2,3,4]))
+print(s.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+print(s.containsDuplicate(None))
+print(s.containsDuplicate([]))
+print(s.containsDuplicate([None]))
+"""
+
+print(s.contains_duplicate_by_sort([1,2,3,1]))
+print(s.contains_duplicate_by_sort([1,2,3,4]))
+print(s.contains_duplicate_by_sort([1,1,1,3,3,4,3,2,4,2]))
+print(s.contains_duplicate_by_sort(None))
+print(s.contains_duplicate_by_sort([]))
+print(s.contains_duplicate_by_sort([None]))
