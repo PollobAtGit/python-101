7187203ed910a2f7d1636ce05c3b2352c204e959
pollobatgit <meet.pollob@gmail.com>
2020-04-26 01:22:36 +0600

    -- templates

---
+++ django/meeting_planner/meetings/models.py
@@ -0,0 +1,22 @@
+from datetime import time
+from django.db import models
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    number = models.IntegerField()
+    floor = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Room {self.number} at floor {self.floor}'
+
+
+class Meetings(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
--- django/meeting_planner/website/views.py
+++ django/meeting_planner/website/views.py
@@ -1,10 +1,20 @@
+
+from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from datetime import datetime
 
 
 def welcome(request):
-    return HttpResponse('welcome to meeting planner')
+    content = {
+            'message' : 'welcome message from plurasight course',
+            'content_url' : 'https://app.pluralsight.com/course-player?clipId=a46b1c63-ed7d-427b-882f-cccdf6e65a11',
+            'date'    : datetime.now()
+            }
+
+    return render(request, 'website/welcome.html', content)
+    #return HttpResponse('welcome to meeting planner')
 
 
 def now(request):
@@ -14,11 +24,4 @@ def now(request):
 def about(request):
     name = request.GET.get('name', '')
     return HttpResponse(f'i am {name}')
-
-
-# add model app but for now
-
-class Meeting:
-    pass
-
 
