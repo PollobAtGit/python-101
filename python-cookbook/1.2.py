e987a2feed72f82708665f93df8311258cdc4440
PollobAtGit <meet.pollob@gmail.com>
2020-07-22 04:45:09 +0600

    -- unpacking - dequeue - yield

--- python-cookbook/1.2.py
+++ python-cookbook/1.2.py
@@ -19,5 +19,21 @@ for operation, *args in records:
         do_bar(args)
 
 
+## SUM WITH UNPACKING ELEMENTS
+
+def sum(item_list):
+    head, *tail = item_list
+    return head + sum(tail) if tail else head 
+
+
+print(sum([1, 2, 3]))
+
+## SPLIT FROM PATTERNED TUPLE
+
+def find_month_year(csvLine):
+    _, (_, _), year, (*month_date, amount) = csvLine
+    return year, month_date[0]
+
+print(find_month_year((None, (None, None), 34, ([5, 6, 7, 's']))))
 
 
---
+++ python-cookbook/1.3.py
@@ -0,0 +1,36 @@
+## FIXED SIZE DEQUE DROPS THE 1ST ELEMENT ADDED 
+
+from collections import deque
+from random import *
+
+def deque_drops_first_element():
+    dq = deque(maxlen=3)
+    
+    # following will be dropped when new item is inserted that exceeds length
+    dq.append(1)
+    dq.append(2)
+    dq.append(3)
+    dq.append(4)
+
+    print(dq)
+
+def yielding_random_numbers():
+    for x in range(10):
+        num = random()
+        yield num
+
+        # print statement will be executed after each 'yield' 
+        print("prnt: " + str(num))
+
+def random_numbers(n=5):
+    for x in range(n):
+        yield random()
+
+def print_random_number():
+    for random_number in random_numbers():
+        print("Current random number: " + str(random_number))
+
+deque_drops_first_element()
+print(list(yielding_random_numbers()))
+print_random_number()
+
